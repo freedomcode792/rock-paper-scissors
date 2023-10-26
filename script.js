00ee0ed3d45bb65cfee74f358207a41b9eb45866
A.G. <142182227+freedomcode792@users.noreply.github.com>
2023-10-26 23:41:05 +0300

    Write some preparatory comments.
    Stop at planning the function responsible for playing
    a single round of the game.

---
+++ script.js
@@ -0,0 +1,32 @@
+/*Rock, Paper, Scissors Game*/
+
+/*Notes:
+    - Commit early and often.
+    - Make sure you understand the assignment.
+    - Plan your solution.
+    - Break big problems down.
+    - Reduce the problems you can't solve to something close but
+        simpler.
+    - Do your best to write your code in self-explanatory manner.
+*/
+
+/* 
+    1) Understand the problem
+*/
+
+/*Plan:
+    1. Write a function that randomly returns into the browser 
+    console either "Rock","Paper" or "Scissors". This function will
+    provide the computer input.
+
+    2. Write a function that plays a single round of Rock, Paper. Scissors
+    The function should take two parameters: playerSelection and 
+    computerSelection and should return a message declaring the
+    winner.
+        1) Output a message such as "rock, paper, scissors!" or something 
+        like that, to make the user aware that the round has starter.
+        2) Prompt the user for 
+
+*/
+
+console.log("Hello World!")
