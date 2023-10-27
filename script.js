54694f48c21af7c56b0173d10b589f449431ae56
A.G. <142182227+freedomcode792@users.noreply.github.com>
2023-10-27 15:49:45 +0300

    Finish getComputerChoice function
    The function randomly outputs one of the three strings: "Rock",
    "Paper" or "Scissors".

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
     computerSelection and should return a message declaring the
     winner.
         1) Output a message such as "rock, paper, scissors!" or something 
-        like that, to make the user aware that the round has starter.
+        like that, to make the user aware that the round has started.
         Wrap this into a function.
         2) Prompt the user for input. Input needs to be checked for
         validity. Both of these can be wrapped into functions, but the
@@ -33,7 +33,18 @@
         winner. Output the result of the match as a string. All of this
         can be done in one function we will call "decideWinner"
 
-    
+
 */
 
 console.log("Hello World!")
+
+function generateComputerChoice(){
+    /*should randomly output one of three strings:"Rock", "Paper"
+    or "Scissors" */
+    /*first get randomly one of the three numbers: 0,1 or 2 */
+    const rand = Math.floor((Math.random()*3));
+    return rand===0 ? "Rock" : rand===1 ? "Paper" : "Scissors";
+    
+}
+
+console.log(generateComputerChoice())
